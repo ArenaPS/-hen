cd589645c6eea953a9c5982177388fbad09afa5b
ArenaPS <75461585+ArenaPS@users.noreply.github.com>
2020-12-04 22:13:28 -0800

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -1,7 +1,7 @@
 document.getElementById("navbar1").innerHTML = `
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<img src="https://arenaps.github.io/icon.png" width="50" height="50">
+			<img src="https://arenaps.github.io/images/icon.png" width="50" height="50">
 			&nbsp;&nbsp;&nbsp;
 			<a class="navbar-brand" href="#">Arena PlayStation</a>
 			<button onclick="function1()" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
